7b417786957764e7d94990c126a1ce624eee3eea
himanshuxx2 <63396352+himanshuxx2@users.noreply.github.com>
2020-04-12 16:43:32 +0530

    Update code.c

--- code.c
+++ code.c
@@ -15,47 +15,29 @@ double times_page_modified_per;
 	
 
 void main(){
-	int swtch;
 	
-	do{
-	
-	
-	printf("Select the required option \n");
-	printf("1.PageFault Rate\n");
-	printf("2.Exit");
-	scanf("%d",&swtch);
-	switch(swtch){
-		case 1:userInput();break;
-		case 2:exit(0);
-	}
-	printf("\n\n");
-
-}while(swtch<3);
+	userInput();
 }
 void userInput(){
 	
 	
-	printf("\nEnter service Page Fault ");
+	printf("\nEnter service Page Fault [not Modified]:");
 	scanf("%lf",&service_page_fault_empty);
-	printf("Enter Service Page Fault ");
+	printf("Enter Service Page Fault [Modified]:");
 	scanf("%lf",&service_page_fault_modified);
-	printf("Enter Memory Access Time");
+	printf("Enter Memory Access Time[in nanoseconds]:");
 	scanf("%lf",&mem_access_time);
-	printf("Enter Percentage of time the page to be replaced is modified[0-100]");
+	printf("Enter Percentage of time the page to be replaced:");
 	scanf("%lf",&times_page_modified);
-	printf("Enter Effective Access time");
+	printf("Enter Effective Access time[0-200]:");
 	scanf("%lf",&effective_access_time);
 
 	service_page_fault_empty_ns = (service_page_fault_empty*1000000);
 	service_page_fault_modified_ns = (service_page_fault_modified*1000000);
 	times_page_modified_per = (times_page_modified/100);   
-	printf("\nPage Fault rate calculated For:\n");
-	printf("Service Page Fault=%lf \n",service_page_fault_empty_ns);
-	printf("Service Page Fault  %lf \n",service_page_fault_modified_ns);
-	printf("Memory Access Time%lf\n",mem_access_time);
-	printf("Effective Access Time %lf\n",effective_access_time);
+	printf("\n***Page Fault rate calculated\n***");
     pageFaultRate =  page_fault_rate(service_page_fault_empty_ns,service_page_fault_modified_ns,mem_access_time,times_page_modified_per,effective_access_time);
-	printf("\nMaximum Acceptable Page Fault rate = %.2e",pageFaultRate);
+	printf("\nMaximum Acceptable Page Fault rate = %.2e[exponential notation]",pageFaultRate);
 
 
 }
